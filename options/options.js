e90ceaf2c2d813d050e1083890c8b73634bef3d1
Crowdbotics <team@crowdbotics.com>
2023-09-14 13:08:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "super-recipe-43386",
+  url: "https://super-recipe-43386.botics.co",
+  api: "https://super-recipe-43386.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
